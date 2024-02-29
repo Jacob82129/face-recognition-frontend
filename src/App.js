5f1e24ac5e47ac3cde985b89a8b625175fba3bff
Jacob Navarro <navarrojacob532@gmail.com>
2024-02-28 21:25:13 -0800

    added backend access from frontend

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ class App extends React.Component {
     this.setState({imageUrl: this.state.input});
 
 
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://fr-backend-084d.onrender.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -120,7 +120,7 @@ class App extends React.Component {
     //     .then(response => response.json())
         .then(response => {
           if(response) {
-            fetch('http://localhost:3000/image', {
+            fetch('https://fr-backend-084d.onrender.com/image', {
               method: 'put',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({
